9bf8a27a9a00d9a23c8582f19993edb03fc15415
simd-float4x4 <asobu.developers+mathew@gmail.com>
2025-10-21 22:44:43 +0900

    fix: ★カテゴリの追加

--- BakusokuToDoList/Modules/List/TodoListView.swift
+++ BakusokuToDoList/Modules/List/TodoListView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import RealmSwift
 
 enum SectionTitle: CaseIterable {
+    case STAR
     case ALL
     case NORMAL
     case CHECKED
@@ -16,6 +17,7 @@ enum SectionTitle: CaseIterable {
     
     var titleText: String {
         switch self {
+        case .STAR: return "★"
         case .ALL: return "すべて"
         case .NORMAL: return "未達成"
         case .CHECKED: return "チェック済"
